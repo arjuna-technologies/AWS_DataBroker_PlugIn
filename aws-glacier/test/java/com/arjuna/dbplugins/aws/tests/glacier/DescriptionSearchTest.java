af4fd32a3eda4b1ae9bd642398ec834e7d6760db
Stuart Wheater <stuart.wheater@arjuna.com>
2015-02-09 22:24:49 +0000

    Skeliton for tests

---
+++ aws-glacier/test/java/com/arjuna/dbplugins/aws/tests/glacier/DescriptionSearchTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2015, Arjuna Technologies Limited, Newcastle-upon-Tyne, England. All rights reserved.
+ */
+
+package com.arjuna.dbplugins.aws.tests.glacier;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DescriptionSearchTest
+{
+    @Test
+    public void archiveDataTest()
+    {
+    }
+}
