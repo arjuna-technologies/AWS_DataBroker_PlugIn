1580f1813f3a92d5fe633944f1990fa133a82283
Stuart Wheater <stuart.wheater@arjuna.com>
2015-02-10 15:12:41 +0000

    Update test

--- aws-glacier/src/test/java/com/arjuna/dbplugins/aws/tests/glacier/DescriptionSearchTest.java
+++ aws-glacier/src/test/java/com/arjuna/dbplugins/aws/tests/glacier/DescriptionSearchTest.java
@@ -4,13 +4,32 @@
 
 package com.arjuna.dbplugins.aws.tests.glacier;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DescriptionSearchTest
 {
+    private static final Logger logger = Logger.getLogger(DescriptionSearchTest.class.getName());
+
     @Test
     public void archiveDataTest()
     {
+        try
+        {
+            AWSAPIProperties awsAPIProperties = new AWSAPIProperties("awsapi.properties");
+
+            if (! awsAPIProperties.isLoaded())
+            {
+                logger.log(Level.INFO, "SKIPPING TEST 'archiveDataTest', no propertiles file");
+                return;
+            }
+        }
+        catch (Throwable throwable)
+        {
+            logger.log(Level.WARNING, "Problem in 'archiveDataTest'", throwable);
+            fail("Problem in 'archiveDataTest': " + throwable);
+        }
     }
 }
